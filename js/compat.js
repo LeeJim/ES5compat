3b7a9387a30543aa13366aa063f8dd4ef9775450
qiulijun <273743552@qq.com>
2015-12-25 18:05:28 +0800

    add ES5's array function

---
+++ js/compat.js
@@ -0,0 +1,214 @@
+
+ (function(){
+
+ 	/*
+ 		@Array ECMAscript5 compat
+ 	*/
+
+ 	if( !Array.prototype.indexOf ) {
+ 		
+ 		Array.prototype.indexOf = function(searchElement, fromIndex) {
+ 			// 调用方，参数
+ 			// this? [] / null ---- [] -> return -1; null -> error
+ 			// typeof searchElement
+ 			// typeof fromIndex == int ? fromIndex : +fromIndex
+ 			// fromIndex = Infinity || fromIndex >= len || fromIndex <0 
+ 			if(this === null ) {
+ 				throw new typeError(' "this" is null or not defined');
+ 			}
+
+ 			var O = Object(this);
+
+ 			var len = O.length >>> 0;
+
+ 			if( len == 0 ) return -1; // 如果数组为空直接返回-1
+
+ 			var n = +fromIndex || 0 ;
+
+ 			if( isFinite(n) ) n=0;
+
+ 			if( n>=len ) return -1;
+
+ 			var k = Math.max( n>=0 ? n : len-Math.abs(n) , 0 );
+
+ 			while( k < len ) {
+ 				if( k in O && O[k]==searchElement ) return k;
+ 				k++;
+ 			}
+
+ 			return -1;
+ 		}
+ 	}
+
+ 	if( !Array.prototype.forEach ) {
+
+ 		Array.prototype.forEach = function(callback, context) {
+
+ 			if( this === null || typeof this === 'undefined' ) throw new typeError();
+
+ 			var len = this.length;
+
+ 			if( len==0 ) throw new typeError();
+			
+			var isFunc = Object.prototype.toString.call(callback) === '[object Function]';
+			
+			if( isFunc ) {
+				
+				for(var i=0; i<len; i++) {
+					//if context = undefined or null {context = window}  :auto
+					callback.call(context, this[i], i, this);
+				}
+			}
+
+ 		}
+ 	}
+
+
+ 	if( !Array.prototype.map ) {
+
+ 		Array.prototype.map = function(callback, context) {
+
+ 			var result = [];
+
+ 			if( this === null || typeof this === 'undefined' ) throw new typeError();
+
+ 			var len = this.length;
+
+ 			if( len==0 ) throw new typeError();
+
+ 			var isFunc = Object.prototype.toString.call(callback) === '[object Function]';
+			
+			if( isFunc ) {
+				
+				for(var i=0; i<len; i++) {
+					//if context = undefined or null {context = window}  :auto
+					result.push( callback.call(context, this[i], i, this) );
+				}
+
+				return result;
+			}
+ 		}
+ 	}
+
+
+ 	if( !Array.prototype.every ) {
+
+ 		Array.prototype.every = function(callback, context) {
+
+ 			if( this === null || typeof this === 'undefined' ) throw new typeError();
+
+ 			var len = this.length;
+
+ 			if( len==0 ) throw new typeError();
+
+ 			var isFunc = Object.prototype.toString.call(callback) === '[object Function]';
+			
+			if( isFunc ) {
+				
+				for(var i=0; i<len; i++) {
+					//if context = undefined or null {context = window}  :auto
+					if( !callback.call(context, this[i], i, this) ) return false;
+				}
+
+				return true;
+			}
+ 		}
+ 	}
+
+ 	if( !Array.prototype.some ) {
+
+ 		Array.prototype.some = function(callback, context) {
+
+ 			if( this === null || typeof this === 'undefined' ) throw new typeError();
+
+ 			var len = this.length;
+
+ 			if( len==0 ) throw new typeError();
+
+ 			var isFunc = Object.prototype.toString.call(callback) === '[object Function]';
+			
+			if( isFunc ) {
+				
+				for(var i=0; i<len; i++) {
+					//if context = undefined or null {context = window}  :auto
+					if( callback.call(context, this[i], i, this) ) return true;
+				}
+
+				return false;
+			}
+ 		}
+ 	}
+
+
+ 	if( !Array.prototype.filter ) {
+
+ 		Array.prototype.filter = function(callback, context) {
+
+ 			var result = [];
+
+ 			if( this === null || typeof this === 'undefined' ) throw new typeError();
+
+ 			var len = this.length;
+
+ 			if( len==0 ) throw new typeError();
+
+ 			var isFunc = Object.prototype.toString.call(callback) === '[object Function]';
+			
+			if( isFunc ) {
+				
+				for(var i=0; i<len; i++) {
+					//if context = undefined or null {context = window}  :auto
+					if( callback.call(context, this[i], i, this) ) result.push(this[i]);
+				}
+
+				return result;
+			}
+ 		}
+ 	}
+
+
+ 	if( !Array.prototype.reduce ) {
+
+ 		Array.prototype.reduce = function(callback, initialValue) {
+
+ 			if( this === null || typeof this === 'undefined' ) throw new typeError();
+
+ 			var len = this.length;
+
+ 			var O = Object(this);
+
+ 			if( len==0 ) throw new typeError();
+
+ 			var isFunc = Object.prototype.toString.call(callback) === '[object Function]';
+			
+			if( isFunc ) {
+
+				var isValueSet = false,
+					value;
+
+				if( arguments.length > 1 ) isValueSet = true;
+
+				var func = function(i) {
+					if( !isValueSet ) {
+						value = O[i];
+						func = function(i) {
+							value = callback(value, O[i], i, O);
+						}
+					}
+					else {
+						value = callback(initialValue, O[i], i, O);
+					}
+					
+				};
+
+				for(var i=0; i<len; i++) {
+					//if context = undefined or null {context = window}  :auto
+					func(i);
+				}
+
+				return value;
+			}
+ 		}
+ 	}
+
+ })()
